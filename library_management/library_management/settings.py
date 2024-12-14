2870935efe002d70b50a0cfbb7e56f28f721b7f1
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-12-14 09:23:43 +0200

    first push

--- library_management/library_management/settings.py
+++ library_management/library_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ol-xvk&83rotw%j=j%j4))#0yr91mf2qqadgoqr!%)u$%@k$om'
+SECRET_KEY = 'django-insecure-_$wluy47$ev4ks#b@xtotma^h)11%s3%)5_7j#o0r=v&m@rx8f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
